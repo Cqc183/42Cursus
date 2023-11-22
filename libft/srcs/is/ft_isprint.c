c40a8c82298afd5589f72566b3506143c9f713b3
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-22 19:20:28 -0300

    Update ft_isprint.c

--- libft/srcs/is/ft_isprint.c
+++ libft/srcs/is/ft_isprint.c
@@ -16,7 +16,7 @@ The isprint() function tests for any printing character, including space
 (` ').
 */
 
-int ft_isprint(int c)
+int    ft_isprint(int c)
 {
     if (c >= 32 && c <= 126)
         return (1);
