9b39fe99874c4fae0cb0eb7624d3b44cf0501276
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-22 19:18:14 -0300

    Update ft_isalpha.c

--- libft/srcs/is/ft_isalpha.c
+++ libft/srcs/is/ft_isalpha.c
@@ -16,7 +16,7 @@ The isalpha() function tests for any character for which isupper(3) or
 islower(3) is true.
 */
 
-int ft_isalpha(int c)
+int    ft_isalpha(int c)
 {
     if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
         return (1);
