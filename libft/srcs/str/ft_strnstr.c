ccddbc501a0a595e33337e2dac222240438fe6a7
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-22 19:27:55 -0300

    Update ft_strnstr.c

--- libft/srcs/str/ft_strnstr.c
+++ libft/srcs/str/ft_strnstr.c
@@ -20,7 +20,7 @@ searched.
 
 #include "libft.h"
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t h;
     size_t n;
