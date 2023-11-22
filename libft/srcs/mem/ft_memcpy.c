d24335bf50dc0ef1bc10bbbfa039da5a1cc0fd20
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-22 19:24:09 -0300

    Update ft_memcpy.c

--- libft/srcs/mem/ft_memcpy.c
+++ libft/srcs/mem/ft_memcpy.c
@@ -19,7 +19,7 @@ s1 and s2 might overlap should use memmove(3) instead.
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
     size_t i;
 
